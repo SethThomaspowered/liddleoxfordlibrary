5f48369ed98dc8f8fb0fa5297f503851e67d28d6
Seth Thomas <athomas.seth@gmail.com>
2021-12-22 20:20:14 -0500

    added BookService

--- src/main/java/com/example/liddleoxfordlibrary/controller/BookController.java
+++ src/main/java/com/example/liddleoxfordlibrary/controller/BookController.java
@@ -1,17 +1,21 @@
 package com.example.liddleoxfordlibrary.controller;
 
+import com.example.liddleoxfordlibrary.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api")
 public class BookController {
+    private BookService bookService;
     private static final Logger LOGGER = Logger.getLogger(BookController.class.getName());
     @GetMapping("/books")
-    public  String Books(){
-        return "Here is where you will go to see all of the books.";
+    public List<Book> getBooks(){
+    LOGGER.info("calling getBooks method from controller");
+    return bookService.getBooks();
     }
 }
---
+++ src/main/java/com/example/liddleoxfordlibrary/service/BookService.java
@@ -0,0 +1,4 @@
+package com.example.liddleoxfordlibrary.service;
+
+public class BookService {
+}
