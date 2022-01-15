64293daf35447951df0fc2e7dfe7e35d4179773d
Seth Thomas <athomas.seth@gmail.com>
2022-01-15 03:02:37 -0500

    added book component

--- liddleoxford/src/app/app.module.ts
+++ liddleoxford/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AddbookComponent } from './addbook/addbook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AddbookComponent
   ],
   imports: [
     BrowserModule,
