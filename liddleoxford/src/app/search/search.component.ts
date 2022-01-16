38020b31e39bc6425805d4aa1eb2babe50bba594
Seth Thomas <athomas.seth@gmail.com>
2022-01-16 01:20:14 -0500

    adding search feature to website

--- liddleoxford/src/app/search/search.component.ts
+++ liddleoxford/src/app/search/search.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
-
+  book: string = "";
+  info: any;
+  constructor(private http: HttpClient) { }
+  findBook(book: string){
+    this.http
+    .get(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
+    .subscribe((response) => {
+      console.log(response);
+      this.info = response;
+    });
+  }
   ngOnInit(): void {
   }
 
